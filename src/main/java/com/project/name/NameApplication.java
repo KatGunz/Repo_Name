4ab7dc6c98f38875ea94ee4ae0cecc048032d6d8
Michael_Bauer <michaelxbauer@gmail.com>
2017-10-13 15:40:58 -0400

    setup

---
+++ src/main/java/com/project/name/NameApplication.java
@@ -0,0 +1,12 @@
+package com.project.name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameApplication.class, args);
+	}
+}
